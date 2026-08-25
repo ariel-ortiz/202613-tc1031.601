0b3936b419101a7889fbe1e140e692ed34e771f4
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2026-08-25 12:41:20 -0600

    Add memory layout demonstration code

---
+++ 02. Memory Management/memory_layout.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+// Code
+void some_function()
+{
+
+}
+
+// Unitialized global variables
+int w;
+int z;
+
+// Initialized global variables
+int x = 5;
+int y = 10;
+
+// Read Only global variables
+const int h = 20;
+const int i = 25;
+
+
+int main()
+{
+    // Local (stack) variables
+    int a;
+    int b;
+
+    // Dynamic (heap) objects
+    int *p = new int;
+    int *q = new int;
+
+    std::cout << "Text segment:\n";
+    std::cout << "&some_function = " << reinterpret_cast<void*>(&some_function) << "\n";
+    std::cout << "&main          = " << reinterpret_cast<void*>(&main) << "\n";
+
+    std::cout << "\nRead only segment:\n";
+    std::cout << "&h             = " << &h << "\n";
+    std::cout << "&i             = " << &i << "\n";
+
+    std::cout << "\nInitialized data segment:\n";
+    std::cout << "&x             = " << &x << "\n";
+    std::cout << "&y             = " << &y << "\n";
+
+    std::cout << "\nUninitialized data segment:\n";
+    std::cout << "&w             = " << &w << "\n";
+    std::cout << "&z             = " << &z << "\n";
+
+    std::cout << "\nHeap:\n";
+    std::cout << "p              = " << p << "\n";
+    std::cout << "q              = " << q << "\n";
+
+    std::cout << "\nStack:\n";
+    std::cout << "&a             = " << &a << "\n";
+    std::cout << "&b             = " << &b << "\n";
+
+    delete p;
+    delete q;
+
+    return 0;
+}
