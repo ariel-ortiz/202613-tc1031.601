142875b806a12f060282daa3466b641366fb9aa2
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2026-09-25 18:09:02 +0000

    Add code to demo Stack usage with palindromes and balanced parenthesis

--- 04. Stack/main.cpp
+++ 04. Stack/main.cpp
@@ -1,27 +1,78 @@
 #include <iostream>
 #include "stack.h"
 
+bool is_palindrome(const std::string& phrase)
+{
+    Stack<char> s(phrase.size());
+
+    for (char c : phrase) {
+        s.push(c);
+    }
+
+    for (char c : phrase) {
+        if (c != s.pop()) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool is_balanced(const std::string& input)
+{
+    Stack<char> s(input.size());
+
+    for (char c : input) {
+        switch (c)
+        {
+        case '(':
+        case '[':
+        case '{':
+            s.push(c);
+            break;
+        case ')':
+            if (s.is_empty() or s.pop() != '(') {
+                return false;
+            }
+            break;
+        case ']':
+            if (s.is_empty() or s.pop() != '[') {
+                return false;
+            }
+            break;
+        case '}':
+            if (s.is_empty() or s.pop() != '{') {
+                return false;
+            }
+            break;
+        }
+    }
+    return s.is_empty();
+}
+
 int main()
 {
-    Stack<int> s(4);
+    std::string word = "kayak";
+    std::cout << word << " is a palindrome? " << is_palindrome(word) << "\n";
+    
+    word = "cat";
+    std::cout << word << " is a palindrome? " << is_palindrome(word) << "\n";
 
-    std::cout << "Capacity = " << s.capacity() << "\n";
-    std::cout << "Size = " << s.size() << "\n";
+    word = "step on no pets";
+    std::cout << word << " is a palindrome? " << is_palindrome(word) << "\n";
 
-    s.push(4);
-    s.push(8);
-    s.push(15);
-    s.push(16);
-    // s.push(23);
-
-    std::cout << "Capacity = " << s.capacity() << "\n";
-    std::cout << "Size = " << s.size() << "\n";
-
-    std::cout << s.pop() << "\n";
-    std::cout << s.pop() << "\n";
-    std::cout << s.pop() << "\n";
-    std::cout << s.pop() << "\n";
-    // std::cout << s.pop() << "\n";
+    word = "((){}[])[()]";
+    std::cout << word << " is balanced? " << is_balanced(word) << "\n";
+    word = ")(";
+    std::cout << word << " is balanced? " << is_balanced(word) << "\n";
+    word = "([)]";
+    std::cout << word << " is balanced? " << is_balanced(word) << "\n";
+    word = "((){}[])[()]";
+    std::cout << word << " is balanced? " << is_balanced(word) << "\n";
+    word = "(()";
+    std::cout << word << " is balanced? " << is_balanced(word) << "\n";
+    word = ")";
+    std::cout << word << " is balanced? " << is_balanced(word) << "\n";
 
     return 0;
 }
